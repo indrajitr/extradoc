302b6ae617c21fddcc2f8c3f25ee66f5d6ed65c1
timperrett <hello@timperrett.com>
2011-01-29 14:42:35 +0000

    Added onejar to extradoc

--- project/build/ExtradocProject.scala
+++ project/build/ExtradocProject.scala
@@ -1,6 +1,8 @@
 import sbt._
+import com.github.retronym.OneJarProject
 
-class ExtradocProject(info: ProjectInfo) extends DefaultProject(info)
+class ExtradocProject(info: ProjectInfo) extends DefaultProject(info) with OneJarProject 
 {
   override def compileOptions = Deprecation :: super.compileOptions.toList
+  override def mainClass = Some("com.novocode.extradoc.ExtraDoc")
 }
